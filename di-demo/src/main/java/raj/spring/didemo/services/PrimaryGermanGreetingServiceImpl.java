d94c2da59182ad413a6bb8d2bd2f17eebcc7a953
Raj Rathore <Raj.Rathore@tatacommunications.com>
2017-09-18 22:17:27 +0530

    Default Profile (031).

--- di-demo/src/main/java/raj/spring/didemo/services/PrimaryGermanGreetingServiceImpl.java
+++ di-demo/src/main/java/raj/spring/didemo/services/PrimaryGermanGreetingServiceImpl.java
@@ -4,14 +4,12 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.security.PermitAll;
-
 @Service
 @Primary
-@Profile({"en","default"})
-public class PrimaryGreetingServiceImpl implements GreetingService {
+@Profile("de")
+public class PrimaryGermanGreetingServiceImpl implements GreetingService{
     @Override
     public String sayGreeting() {
-        return "Hello from PrimaryGreetingServiceImpl.";
+        return "Hello form PrimaryGermanGreetingServiceImpl.";
     }
 }
