e62c5149d93cc48d6247547433f454c3a0e1e9ee
Raj Rathore <Raj.Rathore@tatacommunications.com>
2017-09-23 16:00:57 +0530

    Spring XML Configuration (057).

--- jokes-app/src/main/java/raj/spring/jokesapp/JokesAppApplication.java
+++ jokes-app/src/main/java/raj/spring/jokesapp/JokesAppApplication.java
@@ -2,11 +2,13 @@ package raj.spring.jokesapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:chuck-config.xml")
 public class JokesAppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JokesAppApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(JokesAppApplication.class, args);
+  }
 }
