a1ac7757200de34c58985a847184f0e574285095
Raj Rathore <Raj.Rathore@tatacommunications.com>
2017-09-23 16:53:08 +0530

    Using Factory beans (058).

---
+++ di-demo/src/main/java/raj/spring/didemo/services/GreetingServiceFactory.java
@@ -0,0 +1,23 @@
+package raj.spring.didemo.services;
+
+public class GreetingServiceFactory {
+
+  private GreetingRepository greetingRepository;
+
+  public GreetingServiceFactory(GreetingRepository greetingRepository) {
+    this.greetingRepository = greetingRepository;
+  }
+
+  public GreetingService createGreetingService(String lang) {
+    switch (lang) {
+      case "en":
+        return new PrimaryGreetingServiceImpl(greetingRepository);
+      case "es":
+        return new PrimarySpanishGreetingServiceImpl(greetingRepository);
+      case "de":
+        return new PrimaryGermanGreetingServiceImpl(greetingRepository);
+      default:
+        return new PrimaryGreetingServiceImpl(greetingRepository);
+    }
+  }
+}
